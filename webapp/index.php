eeadbaa2a8cacef2a91cb22e3d846c12e14d10d0
Mohammad Asmar (ASAL Technologies Ltd) <v-mohaas@microsoft.com>
2017-12-25 17:25:44 +0200

    Clean project files in addition to connect the ui with the new template

---
+++ webapp/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>HoneyBot Control Panel</title>
+
+<link rel="stylesheet" href="extras/css/index.css">
+
+<script src="extras/js/jquery-1.12.4.min.js"></script>
+<script src="extras/js/controls.js"></script>
+
+</head>
+<body>
+    <p>Controls</p>
+    <div class="box">
+		<div class="div_box top_row">
+			<span class="up_arrow">&#9650;</span>
+		</div> 
+		<div class="div_box middle_row">
+			<span class="left_arrow">&#x25c0;</span>
+			<span class="center_box">&#x2680;</span>
+			<span class="right_arrow">&#9654;</span>
+		</div> 
+		<div class="div_box down_row">
+			<span class="down_arrow">&#9660;</span>
+		</div> 
+	</div>
+</body>
+</html>
