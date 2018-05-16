ecd2cf19f6dbd0fc9a98bc651c16b44da806b2f7
daigoor <daigoor@gmail.com>
2018-05-16 17:02:36 +0000

    fixs

--- webapp/index.php
+++ webapp/index.php
@@ -9,24 +9,11 @@
 <script src="extras/js/jquery-1.12.4.min.js"></script>
 <script src="extras/js/controls.js"></script>
 
-<script>
-var urlBase = "http://"+window.location.hostname+":9090/";
-document.getElementById("stream_src").setAttribute("value", urlBase);
-document.getElementById("vlcEmb").setAttribute("target", urlBase);
-</script>
-
 </head>
 <body>
     <p>Controls</p>
 
-	<OBJECT classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921" codebase="extras/player/axvlc.cab" width="800" height="600" id="vlc" events="True">
-		<param id="stream_src" name="Src" value="" />
-		<param name="ShowDisplay" value="True" />
-		<param name="AutoLoop" value="False" />
-		<param name="AutoPlay" value="True" />
-		<embed id="vlcEmb" target="" type="application/x-google-vlc-plugin" version="VideoLAN.VLCPlugin.2" autoplay="yes" loop="no" width="640" height="480"></embed>
-	</OBJECT>
-
+    <img src="http://localhost:8080/?action=stream">
     <div class="box">
 		<div class="div_box top_row">
 			<span class="up_arrow">&#9650;</span>
